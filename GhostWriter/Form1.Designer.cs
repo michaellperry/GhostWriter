232ca034e2fa892d636dff9ff4b122b277178e6f
xmorera <xavier@familiamorera.com>
2016-05-30 10:40:53 -0600

    Initializes repository with base application

---
+++ GhostWriter/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace AutomaticWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("Century Schoolbook", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(360, 281);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.WordWrap = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Transparent;
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.Location = new System.Drawing.Point(68, 299);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(50, 50);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
+            this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.Location = new System.Drawing.Point(12, 299);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(50, 50);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestart.Image = ((System.Drawing.Image)(resources.GetObject("btnRestart.Image")));
+            this.btnRestart.Location = new System.Drawing.Point(124, 299);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(50, 50);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Schoolbook", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(272, 323);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(273, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Interval (ms)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Form1";
+            this.Text = "Automatic Writer";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GhostWriter/Form1.cs
@@ -0,0 +1,152 @@
+﻿// <author>Xavier Morera & Maikol Barrantes</author>
+// <date>05/30/2016</date>
+// <summary>App to write text while you talk</summary>
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AutomaticWriter
+{
+    public partial class Form1 : Form
+    {
+        public Process p;
+        public IntPtr h;
+            
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        [DllImport("User32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                Process[] devenvs = Process.GetProcessesByName("devenv");
+                //Process[] devenvs = Process.GetProcessesByName("notepad");
+                if (devenvs.Length == 0) return;
+                if (devenvs[0] != null)
+                {
+                    //p = Process.Start("notepad.exe");
+                    p = devenvs[0];
+                    p.WaitForInputIdle();
+                    h = p.MainWindowHandle;
+                    SetForegroundWindow(h);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("There is no target open");
+                this.Close();
+            }
+                        
+            birth = 0;
+            dead = 1;
+            active = false;
+            try
+            {
+                timer1.Interval = Int32.Parse(textBox2.Text);
+            }
+            catch
+            {
+                timer1.Interval = 200;
+            }
+
+        }
+
+        public bool active;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            active = true;
+            try
+            {
+                timer1.Interval = Int32.Parse(textBox2.Text);
+            }
+            catch { }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            active = false;
+            timer1.Enabled = false;
+        }
+
+        int birth;
+        int dead;
+
+        public string[] specialChars = { "{", "}", "(", ")", "+", "^", "[", "]", "*", ";", "=", "_", ".", "&", "|", ",\"", "\'" };
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (birth < textBox1.Text.Length)
+            {
+                bool _specialCharFound = false;
+
+                string arr = textBox1.Text.Substring(birth++, dead);
+                
+                for (int i = 0; i < specialChars.Length; i++)
+                {
+                    if (arr == specialChars[i])
+                    {
+                        _specialCharFound = true;
+                        break;
+                    }
+                }
+
+                if (arr != "\n")
+                    if (_specialCharFound)
+                        SendKeys.Send("{" + arr + "}");
+                    else 
+                        SendKeys.Send(arr);
+
+                /*
+                if (birth > 2 && last_word == textBox1.Text.Substring(birth-2, dead))
+                    SendKeys.SendWait(textBox1.Text.Substring(birth-1, dead));
+                SendKeys.SendWait(textBox1.Text.Substring(birth, dead));
+                last_word = textBox1.Text.Substring(birth++, dead);
+                */
+            }
+            else
+            {
+                timer1.Enabled = false;
+                birth = 0;
+            }            
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            birth = 0;
+            active = false;
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (birth > 0)
+            {
+                textBox2.ReadOnly = true;
+            }
+            else
+            {
+                textBox2.ReadOnly = false;
+            }
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (active)
+            {
+                System.Threading.Thread.Sleep(1500);
+                timer1.Enabled = true;
+            }
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
